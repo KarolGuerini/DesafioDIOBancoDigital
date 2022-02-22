b6a7aeabdbfc2bb94ca7b0514cfc05985a1003c6
KarolGuerini <karoltech20@gmail.com>
2022-02-22 14:42:52 -0300

    first commit

---
+++ src/main/java/ContaCorrente.java
@@ -0,0 +1,36 @@
+public class ContaCorrente {
+
+    private int agency;
+    private int account;
+    private int balance;
+
+    public void withdraw(){
+
+
+    }
+
+    public void deposit(){
+
+
+    }
+
+    public void transfer(){
+
+
+    }
+
+    public int getAgency() {
+        return agency;
+    }
+
+    public int getAccount() {
+        return account;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+
+
+}
---
+++ src/main/java/ContaPoupanca.java
@@ -0,0 +1,21 @@
+public class ContaPoupanca {
+
+    private int agency;
+    private int account;
+    private int balance;
+
+    public void withdraw(){
+
+
+    }
+
+    public void deposit(){
+
+
+    }
+
+    public void transfer(){
+
+
+    }
+}
